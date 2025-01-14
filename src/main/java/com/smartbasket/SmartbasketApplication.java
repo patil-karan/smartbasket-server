fd84ff056ddfa85c14390731f028c67ededd002e
patil-karan <159009334+patil-karan@users.noreply.github.com>
2025-01-14 22:29:54 +0530

    Intial commit for Smartbasket Project

---
+++ src/main/java/com/smartbasket/SmartbasketApplication.java
@@ -0,0 +1,13 @@
+package com.smartbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartbasket/SmartbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
